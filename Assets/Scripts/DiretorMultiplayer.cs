753c7b34e84722f3ea4530e3f3990d893c4db538
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-07 10:08:43 -0300

    video 5.2

--- Assets/Scripts/DiretorMultiplayer.cs
+++ Assets/Scripts/DiretorMultiplayer.cs
@@ -7,17 +7,20 @@ public class DiretorMultiplayer : Diretor {
     private Jogador[] jogadores;
     private bool alguemMorto;
     private int pontosDesdeAMorte;
+    private InterfaceCanvasInativo interfaceInativo;
 
     protected override void Start()
     {
         base.Start();
         this.jogadores = GameObject.FindObjectsOfType<Jogador>();
+        this.interfaceInativo = GameObject.FindObjectOfType<InterfaceCanvasInativo>();
     }
 
-    public void AvisarQueAlguemMorreu()
+    public void AvisarQueAlguemMorreu(Camera camera)
     {
         this.alguemMorto = true;
         this.pontosDesdeAMorte = 0;
+        this.interfaceInativo.Mostrar(camera);
     }
     public void ReviverSePrecisar()
     {
---
+++ Assets/Scripts/InterfaceCanvasInativo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceCanvasInativo : MonoBehaviour {
+    [SerializeField]
+    private GameObject fundo;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        this.canvas = this.GetComponent<Canvas>();
+    }
+    
+    public void Mostrar(Camera camera)
+    {
+        this.fundo.SetActive(true);
+        this.canvas.worldCamera = camera;
+    }
+}
