c028a547c29ebf04813214cd5d363e174448117e
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-08 16:10:28 -0300

    Projeto Final

---
+++ Assets/Scripts/AtivarJogadorComAnimacao.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AtivarJogadorComAnimacao : MonoBehaviour {
+    [SerializeField]
+    private UnityEvent aoTerminarAnimacao;
+    public void AtivarJogador()
+    {
+        this.aoTerminarAnimacao.Invoke();
+    }
+}
